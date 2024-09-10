45809b804ccca8542be776aed8dd9f1a398c9438
jc_jimenez <jcjj.system@gmail.com>
2024-09-10 12:40:47 -0300

    update app.js
    json link updated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 angular.module('resume', []).controller('Controller', ['$scope', '$http', 
 function($scope, $http) {
-	$http.get('https://www.jcjimenez.me/resume.json')
+	$http.get('https://zsyslog.github.io/resume.json')
 						.success(function(data, status, headers, config){
 							console.log(data)
 							$scope.resume = data
