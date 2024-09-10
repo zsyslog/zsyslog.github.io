0b48c3fc2b5450cd98af417c678840b59ae71786
jc_jimenez <jcjj.system@gmail.com>
2024-09-10 13:01:39 -0300

    use json relative URL

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 angular.module('resume', []).controller('Controller', ['$scope', '$http', 
 function($scope, $http) {
-	$http.get('https://zsyslog.github.io/resume.json')
+	// $http.get('https://zsyslog.github.io/resume.json')
+	$http.get('/resume.json')
 						.success(function(data, status, headers, config){
 							console.log(data)
 							$scope.resume = data
