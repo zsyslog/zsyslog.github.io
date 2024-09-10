6d89505f24e03526860a3b6b12b876b5cc7a3e81
jc_jimenez <jcjj.system@gmail.com>
2024-09-10 13:05:59 -0300

    use location.host

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 angular.module('resume', []).controller('Controller', ['$scope', '$http', 
 function($scope, $http) {
 	// $http.get('https://zsyslog.github.io/resume.json')
-	$http.get('/resume.json')
+	var host = window.location.host; // Obtiene el host actual, incluyendo subdominio y puerto si los hay.
+    var url = 'https://' + host + '/resume.json'; // Construye la URL para hacer la solicitud.
+	$http.get(url)
 						.success(function(data, status, headers, config){
 							console.log(data)
 							$scope.resume = data
